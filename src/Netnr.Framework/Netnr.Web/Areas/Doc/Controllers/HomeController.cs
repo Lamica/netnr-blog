13099aa4c3074c0eb9ad4a6b0d98288978f2da87
Lamica <lpw18947663240@outlook.com>
2020-02-09 18:19:42 +0800

    netnr-blog

---
+++ src/Netnr.Framework/Netnr.Web/Areas/Doc/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel;
+
+namespace Netnr.Web.Areas.Doc.Controllers
+{
+    [Area("Doc")]
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// Doc首页
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
